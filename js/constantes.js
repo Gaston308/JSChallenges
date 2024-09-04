0efdb6ff0ccad2cc033fe63da579763d7659902d
Gaston Romero <gaston.leo285@gmail.com>
2024-09-03 23:55:59 -0300

    javascript exercises

---
+++ js/constantes.js
@@ -0,0 +1,4 @@
+export const PI = Math.PI;
+
+export let usuario = "gaston";
+let password = "qwerty";
---
+++ js/modulos.js
@@ -0,0 +1,7 @@
+import { PI, usuario } from "./constantes.js";
+import { sumar, restar } from "./aritmetica.js";
+
+console.log("Archivo modulos.js");
+console.log(PI);
+console.log(usuario);
+console.log(sumar(3, 4));
